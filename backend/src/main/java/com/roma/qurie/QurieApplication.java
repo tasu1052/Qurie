061b2a25e6c54845e3a1abc51854ee3145502195
김태수 <tasu1052@naver.com>
2026-07-24 12:49:04 +0900

    backend 초기 설정 및 폴더 정리

---
+++ backend/src/main/java/com/roma/qurie/QurieApplication.java
@@ -0,0 +1,13 @@
+package com.roma.qurie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QurieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QurieApplication.class, args);
+	}
+
+}
