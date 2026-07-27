9fe7e2157a6d8a6382208bf5b14e9f15432a2ab1
원민관 <a4ueo@naver.com>
2026-07-27 14:17:38 +0900

    feat(frontend): class API 및 hooks 추가

--- frontend/src/network/queryKeys.ts
+++ frontend/src/network/queryKeys.ts
@@ -2,9 +2,12 @@ export const queryKeys = {
     auth: {
         all: ['auth'] as const,
     },
-
     track: {
         all: ['track'] as const,
         list: () => [...queryKeys.track.all, 'list'] as const,
     },
+    class: {
+        all: ['class'] as const,
+        byTrack: (track: string) => [...queryKeys.class.all, track] as const,
+    }
 }
