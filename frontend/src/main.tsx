126325fc28429974e20c9df3116244ff8b5e9437
unknown <ezrahuh7@gmail.com>
2026-07-29 10:29:02 +0900

    feature(frontend): 다크모드 구현

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from './network/core/queryClient.ts'
+import { ThemeProvider } from './theme/ThemeProvider.tsx'
 import './styles/index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>,
 )
